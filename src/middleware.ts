45fcc96e0cfcf5723939a4eec00f5cee4267ea39
bowenzhou222 <bowenzhou222@gmail.com>
2018-11-21 06:53:08 +1100

    fix dependency

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,6 @@
 import * as cors from 'cors';
 import * as helmet from 'helmet';
 import * as bodyParser from 'body-parser';
-import * as expressValidator from 'express-validator';
 import { RequestHandler } from 'express';
 
 export const appMiddlewares: Array<RequestHandler> = [
@@ -9,5 +8,4 @@ export const appMiddlewares: Array<RequestHandler> = [
   helmet(),
   bodyParser.urlencoded({ extended: true }),
   bodyParser.json(),
-  expressValidator(),
 ];
